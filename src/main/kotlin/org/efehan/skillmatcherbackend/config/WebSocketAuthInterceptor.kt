01e388bef3b5c8414893846ddce3faededcf8b61
ckc-efehan <Efehan.Cekic@Student.HTW-Berlin.de>
2026-02-16 19:44:17 +0100

    feat: add configurations

---
+++ src/main/kotlin/org/efehan/skillmatcherbackend/config/WebSocketAuthInterceptor.kt
@@ -0,0 +1,48 @@
+package org.efehan.skillmatcherbackend.config
+
+import org.efehan.skillmatcherbackend.core.auth.CustomUserDetailsService
+import org.efehan.skillmatcherbackend.core.auth.JwtService
+import org.efehan.skillmatcherbackend.shared.exceptions.InvalidTokenException
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.messaging.support.MessageHeaderAccessor
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity
+
+@Configuration
+@EnableWebSocketSecurity
+class WebSocketAuthInterceptor(
+    private val jwtService: JwtService,
+    private val userDetailsService: CustomUserDetailsService,
+) : ChannelInterceptor {
+    override fun preSend(message: Message<*>, channel: MessageChannel): Message<*> {
+        val accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java)
+
+        if (accessor != null && accessor.command == StompCommand.CONNECT) {
+            val token = accessor.getFirstNativeHeader("Authorization")
+                ?.removePrefix("Bearer ")
+                ?.ifBlank { null }
+
+            if (token != null) {
+                try {
+                    val email = jwtService.getEmail(token)
+                    val userDetails = userDetailsService.loadUserByUsername(email)
+
+                    if (userDetails.isEnabled) {
+                        accessor.user = UsernamePasswordAuthenticationToken(
+                            null,
+                            userDetails.authorities
+                        )
+                    }
+                } catch (_: InvalidTokenException) {
+
+                }
+            }
+        }
+        return message
+    }
+}
